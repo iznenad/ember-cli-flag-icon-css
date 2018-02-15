9ab07f9e59411f51374ae9543d3522fef0301dda
Nenad Nikolic <nenad.nikolic@ebf.de>
2018-02-15 10:58:38 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-cli-flag-icon-css'
+    name: 'ember-cli-flag-icon-css',
+    treeForPublic(publicTree) {
+
+        let moduleFolder = path.dirname(require.resolve('flag-icon-css/index.html'));
+
+        var flagsFunnel = new Funnel(
+            moduleFolder, {
+                srcDir: 'flags',
+                destDir: path.join('assets', 'flags')
+            }
+        );
+        var cssFunnel = new Funnel(
+            moduleFolder, {
+                srcDir: 'css',
+                destDir: path.join('assets', 'flags-css')
+            }
+        );
+
+
+        return new MergeTrees([publicTree, flagsFunnel, cssFunnel].filter(Boolean));
+    }
 };
